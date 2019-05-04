ce1046c8c74d5041235931aa058699288be2ef8d
Steven Ma <mshaobo@bu.edu>
2019-05-04 23:24:37 +0800

    Rename project.

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to my-angular-app!');
+    expect(page.getTitleText()).toEqual('Welcome to learning-notes-app!');
   });
 
   afterEach(async () => {
--- src/karma.conf.js
+++ src/karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function (config) {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, '../coverage/my-angular-app'),
+      dir: require('path').join(__dirname, '../coverage/learning-notes-app'),
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
